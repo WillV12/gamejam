321371c63e1b94bcc5cd03c43690c616be658771
carolbenhami <145382917+carolbenhami@users.noreply.github.com>
2024-03-06 13:10:08 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,53 @@
+respect_level= 50
+# below 30, above 70, danger zone
+# server
+
+
+
+for days in range(1,6):
+      pass
+
+
+def bitcoin():
+    pass
+def chatgpt():
+    pass
+def roblox():
+    pass
+def food():
+    pass
+def spill_coffee():
+    pass
+def burn():
+    pass
+def upgrade():
+    pass
+def admin_access():
+    pass
+def office_ransack():
+    pass
+def lanschool():
+    pass
+
+office = [admin_access(), office_ransack(), lanschool()]
+room_251 = [chatgpt(), roblox(), food(), bitcoin()]
+server = [spill_coffee(), burn(), upgrade() ]
+
+def software_unaccess():
+    pass
+def slow_wifi():
+    pass
+def servers_down():
+    pass
+def online_textbook():
+    pass
+def wiped_hard_drives():
+    pass
+
+issues = [software_unaccess(), slow_wifi(), servers_down(), online_textbook(), wiped_hard_drives()]
+
+
+
+
+
+
