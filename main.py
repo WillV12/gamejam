4a93003f74a7aceb707a2c711cbfc15eda45ecf5
Will Vanderploeg <thatmurphyboi@gmail.com>
2024-03-06 12:27:05 -0500

    Initial commit
    Yippee

---
+++ main.py
@@ -0,0 +1,8 @@
+
+name = input("What is your name?:\n")
+
+print(f"Hello {name}, you will be in the role of Clint Matthews, the IT admin of Wake Tech, for the upcoming week"
+      f"\nYou have the duty of ensuring the "
+      f"students safety and productivity in class. \nMake sure they follow the code of conduct, don't run into issues, "
+      f"or break Wake Tech property\nGood luck, {name}.\n\nYou will need it...")
+
