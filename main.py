c34a6bef56fafd3a62fdf2c2dd4a1727aee9d544
carolbenhami <145382917+carolbenhami@users.noreply.github.com>
2024-03-07 12:09:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ print(f"Hello {name}, you will be in the role of Clint Matthews, the IT admin of
       f"students safety and productivity in class. \nMake sure they follow the code of conduct, don't run into issues, "
       f"or break Wake Tech property\nGood luck, {name}.\n\nYou will need it...")
 
+input("Press any key to continue")
+print("")
+print("")
+print("")
+print("")
+print ("It is Monday, students are unhappy because they are back to school. \nMr. McCuen, the professor, needs students to use Wireshark to complete one of the NOS 110 assignments.\nGuess what? None of the students have access to Wiresahark.\nYou need to make sure all the computers have Wireshark installed as fast as possible because students are waiting in class. ")
+input("Press any key to continue")
+print("")
+print("")
+print("")
+print("")
